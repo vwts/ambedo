2802de87e22bfe24356e7bcfcfa8925e4ca2f823
grivo <gyket.pentest.133971@protonmail.com>
2024-02-02 10:35:48 -0300

    módulos não mais atualizados

---
+++ Lib/lib-old/util.py
@@ -0,0 +1,9 @@
+# módulo 'util' -- algumas funções úteis
+
+# remove um item de uma lista
+def remove(item, list):
+    for i in range(len(list)):
+        if list[i] = item:
+            del list[i]
+            
+            break
